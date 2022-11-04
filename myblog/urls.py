044b3e310bedb8a14be404d2b0b41ddd81a55882
danish703 <dipu.danish@outlook.com>
2022-11-04 19:52:49 +0545

    authenticated added

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,12 +1,15 @@
-
 from django.contrib import admin
 from django.urls import path
-from .views import home,details
+from .views import home, details, signup, signin, dashboard,signout
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',home,name="home"),
-    path('details/<int:id>',details,name='details')
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', home, name="home"),
+                  path('details/<int:id>', details, name='details'),
+                  path('signup/', signup, name='signup'),
+                  path('signin/', signin, name='signin'),
+                  path('signout/', signout, name='signout'),
+                  path('dashboard/', dashboard, name='dashboard'),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- myblog/views.py
+++ myblog/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from blog.models import Blog
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login,authenticate,logout
 def home(request):
     blogs = Blog.objects.all() #select * from blogs
     context = {
@@ -13,3 +16,47 @@ def details(request,id):
         'blog':blog
     }
     return render(request,'details.html',context)
+
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+        message = ""
+        if pass1 == pass2:
+            user = User(username=username)
+            user.set_password(pass1)
+            user.save()
+            msg="Save successfully"
+            messages.add_message(request,messages.SUCCESS,msg)
+            return redirect('signin')
+        else:
+            msg="Password and Password Confirmation does not match"
+            messages.add_message(request,messages.ERROR,msg)
+            return redirect('signup')
+    else:
+        return render(request,'signup.html')
+
+def signin(request):
+    if request.method=='POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        user = authenticate(username=username,password=pass1)
+        if user is None:
+            msg = "username and password does not match"
+            messages.add_message(request,messages.ERROR,msg)
+        else:
+            login(request,user)
+            return redirect('dashboard')
+    return render(request,'signin.html')
+
+
+def dashboard(request):
+    if request.user.is_authenticated:
+        return render(request,'dashboard.html')
+    else:
+        return redirect('signin')
+
+def signout(request):
+    logout(request)
+    return redirect('signin')
